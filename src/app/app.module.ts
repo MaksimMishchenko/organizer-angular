25034cf669f6074c1f97dbad3605840ee08fb0eb
Maksim Mishchenko <programm532@ukr.net>
2022-01-26 19:49:35 +0200

    feat: add organizer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { CalendarComponent } from './calendar/calendar.component';
-import { SelectorComponent } from './selector/selector.component';
-import { OrganizerComponent } from './organizer/organizer.component';
+import {AppComponent} from './app.component';
+import {CalendarComponent} from './calendar/calendar.component';
+import {SelectorComponent} from './selector/selector.component';
+import {OrganizerComponent} from './organizer/organizer.component';
 import {MomentPipe} from "../shared/moment.pipe";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -16,11 +18,13 @@ import {MomentPipe} from "../shared/moment.pipe";
     MomentPipe,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [
-
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/organizer/organizer.component.ts
+++ src/app/organizer/organizer.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {DateService} from "../../shared/date.service";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Task, TaskService} from "../../shared/task.service";
+import {switchMap} from "rxjs";
 
 @Component({
   selector: 'app-organizer',
@@ -7,9 +11,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrganizerComponent implements OnInit {
 
-  constructor() { }
+  // @ts-ignore
+  form: FormGroup;
+  tasks: Task[] = [];
+
+  constructor(public dateService: DateService, private taskService: TaskService) { }
 
   ngOnInit(): void {
+    this.dateService.date.pipe(
+      switchMap(value => this.taskService.loadAllTasks(value))
+    ).subscribe(tasks => {
+      this.tasks = tasks;
+    });
+
+    this.form = new FormGroup({
+      title: new FormControl('', Validators.required),
+    });
   }
 
+  submit() {
+    const { title } = this.form?.value;
+
+    const task: Task = {
+      title,
+      date: this.dateService.date.value.format('DD-MM-YYYY')
+    };
+
+    this.taskService.create(task).subscribe(task => {
+      this.tasks.push(task);
+      this.form.reset();
+    }, err => { console.error(err); });
+  }
+
+  remove(task: Task) {
+    this.taskService.remove(task).subscribe(() => {
+      this.tasks = this.tasks.filter(t => t.id !== task.id);
+    }, error => console.error(error));
+  }
 }
